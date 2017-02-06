bea1dab98bf4b40a6a95e38751f4a163496d6b7f
Rainer Oehlmann <rainer.oehlmann@gmx.de>
2017-02-06 17:59:34 +0100

    Add dynamic version of the projekte-all page

--- wp-content/themes/fkc/archive-projekte.php
+++ wp-content/themes/fkc/archive-projekte.php
@@ -1,51 +1,208 @@
 <?php
-/**
- * The template for displaying archive pages
- *
- * @link https://codex.wordpress.org/Template_Hierarchy
- *
- * @package Fernkopie_Custom
- */
+/*
+	Template Name: Projekte Page
+*/
+
+ 	// Collect the Category-links
+	
+    // Get the ID of a given category
+    $category_id = get_cat_ID( 'Visuelle Identitäten' );
+    // Get the URL of this category
+    $link_vis_ident = get_category_link( $category_id );
+    
+    // Get the ID of a given category
+    $category_id = get_cat_ID( 'Informationsmedien' );
+    // Get the URL of this category
+    $link_info_med = get_category_link( $category_id );  
+    
+    // Get the ID of a given category
+    $category_id = get_cat_ID( 'Schrift im Raum' );
+    // Get the URL of this category
+    $link_schrift_raum = get_category_link( $category_id );  
+    
+    // Get the ID of a given category
+    $category_id = get_cat_ID( 'Editorial' );
+    // Get the URL of this category
+    $link_editorial = get_category_link( $category_id );  
+    
+	// Get the ID of a given category
+    $category_id = get_cat_ID( 'Plakat' );
+    // Get the URL of this category
+    $link_plakat = get_category_link( $category_id );  
+    
+    // Get the ID of a given category
+    $category_id = get_cat_ID( 'Briefmarken' );
+    // Get the URL of this category
+    $link_briefmarken = get_category_link( $category_id );  
+
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
 
-		<?php
-		if ( have_posts() ) : ?>
 
-			<header class="page-header">
-				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
+<!-- CONTENT
+===================================================== -->
+<div id="contentmain">
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
+	<!-- Projectsheader
+    ===================================================== -->
+	<div id="projectsheader" class="container">
+		
+		<div class="row">
+			<div class="col-xs-8 col-lg-10 col-lg-offset-1">
+				<h1 class="vh1 l-bold">Unsere Projekte</h1>
+				<p class="vh2">hier wird ein kurzer erklärender Text zu der jeweiligen Kategorie stehen</p>
+			</div><!-- /.col -->
+		</div><!-- /.row -->
+			
+	</div><!-- /.container -->
 
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
 
-			endwhile;
+	<!-- Projekte
+    ===================================================== -->
+    <div id="projectsheader" class="container">
+		
+		<div class="row">
+			<div class="col-xs-8 col-lg-10 col-lg-offset-1">
+				<h2 class="vh6">ausgewählte Projekte</h2>
+			</div><!-- /.col -->
+		</div><!-- /.row -->
+		
+	</div><!-- /.container -->
+	
+	
+    
+    <div id="projects-section" class="container">
+	    
+		<?php 
+	    //Display the contents
+	    if ( have_posts() ) : ?>
+    
+			<!-- Projektauswahlmatrix -->
+			<div class="row">
+	    
+			    <?php while ( have_posts() ) : the_post();
+			    	// Könnte ich auch direkt in den Loop mit einbauen mit einer meta_query 
+			    	$show_as				= get_field('show_as');
+			    	
+			    	if ( $show_as != 'pic' ) { continue; }
+			    	
+			    	$customer						= get_field('customer');
+			    	$project_title 					= get_field('project_title');
+			    	$feature_image					= get_field('feature_image'); 
+			    ?>
+		
+				<div class="col-xs-12 col-lg-6">
+					<figure class="gallery-item">
+						<a href="<?php echo get_permalink();?>" >
+							<img src="<?php echo $feature_image['url']; ?>" alt="<?php echo $feature_image['alt']; ?>" >
+							<figcaption>
+								<h3 class="l-bold vh6"><?php echo $customer; ?></h3>
+								<p class="vh6"><?php echo $project_title; ?></p>
+							</figcaption>
+						</a>
+					</figure>
+				</div><!-- /.col -->
+		
+				<?php endwhile; ?>
+				
+			</div><!-- /.row -->
+		<?php endif; ?>
+		<?php rewind_posts(); ?>
+		
+			<!-- Weitere Projekte
+	        ===================================================== -->
+			<?php if ( have_posts() ) : ?>
+				<div class="projects-all-list">
+					<div class="row">
+						<div class="col-xs-11 col-xs-offset-1 col-lg-10 col-lg-offset-1">
+							<p class="vh6">mehr Projekte</p>
+							<ul class="l-list-no-bullets l-inline">
+								<?php while ( have_posts() ) : the_post();
+						    	// Könnte ich auch direkt in den Loop mit einbauen mit einer meta_query 
+						    	$show_as				= get_field('show_as');
+						    	
+						    	if ( $show_as != 'text' ) { continue; }
+						    	
+						    	$customer						= get_field('customer');
+						    	$project_title 					= get_field('project_title');
+						    	$feature_image					= get_field('feature_image'); 
+						    	?>
+						    	
+						    		<li class="l-inline">
+						    			<p class="vh2 l-bold l-inline"><a href="<?php echo get_permalink(); ?>" ><?php echo $customer; ?></a></p>
+						    		</li>
+							    								
+								<?php endwhile; ?>
+							</ul><!-- /.l-list-no-bullets -->
+						</div><!-- /.col -->	
+					</div><!-- /.row -->
+				</div><!-- /.projects-all-list -->
+			<?php endif; ?>
 
-			the_posts_navigation();
+				
+		</div><!-- /#projects-section .container -->
+	    
+	
 
-		else :
 
-			get_template_part( 'template-parts/content', 'none' );
 
-		endif; ?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
 
+
+	<!-- Archiv
+    ===================================================== -->
+	<div id="archiv-selection-section">
+	
+		<div class="container archiv-selection">
+			
+			<div class="row">
+				<a class="is-hidden-lg" href="#"><img class="logo l-center-img" src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/fernkopie-logo.jpg" alt="Fernkopie Logo" title="Fernkopie Logo"></a>
+			</div><!-- /.row -->
+			
+			<div class="row">
+				<div class="col-xs-4 archiv-selection-col">
+					<img class="logo l-center-img is-displayed-lg" src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/fernkopie-logo.jpg" alt="Fernkopie Logo" title="Fernkopie Logo">
+					<a href="archiv-category.html">
+						<p class="l-center-text l-uppercase vh4">Archiv Editorial</p>
+						<div class="row">
+							<div class="col-lg-4 col-lg-offset-4">
+								<img class="l-center-img" src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/beispielbilder/belin-1.jpg" alt="">
+							</div><!-- /.col -->
+						</div><!-- /.row -->
+					</a>
+				</div><!-- /.col -->
+					
+				<div class="col-xs-4 archiv-selection-col">
+					<img class="logo l-center-img is-displayed-lg" src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/fernkopie-logo.jpg" alt="Fernkopie Logo" title="Fernkopie Logo">
+					<a href="archiv-category.html">
+						<p class="l-center-text l-uppercase vh4">Archiv Plakate</p>
+						<div class="row">
+							<div class="col-lg-4 col-lg-offset-4">
+								<img class="l-center-img" src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/beispielbilder/belin-2.jpg" alt="">
+							</div><!-- /.col -->
+						</div><!-- /.row -->
+					</a>
+				</div><!-- /.col -->
+					
+				<div class="col-xs-4 archiv-selection-col">
+					<img class="logo l-center-img is-displayed-lg" src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/fernkopie-logo.jpg" alt="Fernkopie Logo" title="Fernkopie Logo">
+					<a href="archiv-category.html">
+						<p class="l-center-text l-uppercase vh4">Archiv Briefmarken</p>
+						<div class="row">
+							<div class="col-lg-4 col-lg-offset-4">
+								<img class="l-center-img" src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/beispielbilder/belin-3.jpg" alt="">
+							</div><!-- /.col -->
+						</div><!-- /.row -->
+					</a>
+				</div><!-- /.col -->
+			</div><!-- /.row -->	
+			
+		</div><!-- /.container .archiv-selection -->
+	
+	</div><!-- /#archiv-selection-section -->
+</div><!-- /#contentmain -->
+
+	
 <?php
-get_sidebar();
 get_footer();
