b533c88bb08ddeb656d359178ca3d176bfa37f59
Rainer Oehlmann <rainer.oehlmann@gmx.de>
2017-02-12 23:03:56 +0100

    Make the archive-category-links remove a category if we are on an archive-category site

--- wp-content/themes/fkc/archive-archiv.php
+++ wp-content/themes/fkc/archive-archiv.php
@@ -3,6 +3,22 @@
 	Template Name: Archiv Page
 */
 
+// Get the ID of a given category
+$category_id = get_cat_ID( 'Editorial' );
+// Get the URL of this category
+$link_editorial = get_category_link( $category_id );  
+
+// Get the ID of a given category
+$category_id = get_cat_ID( 'Plakat' );
+// Get the URL of this category
+$link_plakat = get_category_link( $category_id );  
+
+// Get the ID of a given category
+$category_id = get_cat_ID( 'Briefmarken' );
+// Get the URL of this category
+$link_briefmarken = get_category_link( $category_id );  
+
+
 get_header(); ?>
 
 
@@ -13,14 +29,14 @@ get_header(); ?>
 
 	<!-- Archiv
     ===================================================== -->
-	<div id="archiv-selection-section">
+	
 		
 		<!-- Archive-header
 	    ===================================================== -->
 		<div id="archivesheader" class="container">
 			
 			<div class="row">
-				<div class="col-xs-8 col-lg-10 col-lg-offset-1">
+				<div class="col-xs-10 col-lg-10 col-lg-offset-1">
 					<h1 class="vh1 l-bold l-not-bold-lg">Fernkopie Archiv</h1>
 					<p class="vh2">Hier wird ein kurzer Text zum Archiv stehen. Wahrscheinlich zwei- oder dreizeilig. Hier wird ein kurzer Text zum Archiv stehen. Wahrscheinlich vier- oder fünfzeilig.</p>
 				</div><!-- /.col -->
@@ -30,12 +46,53 @@ get_header(); ?>
 
 
 
-		<!-- Archive-Selection
-	    ===================================================== -->
-		<?php get_template_part('template-parts/archiv', 'selection'); ?>
-		
+	<!-- Archive-Selection
+	===================================================== -->
+	<div id="archiv-selection-section">
+	
+				
+		<div class="container archiv-selection archiv-page">
+						
+			<div class="row">
+				<div class="col-xs-4 archiv-selection-col no-border-lg">
+					<a href="<?php echo esc_url( $link_editorial ); ?>">
+						<p class="l-center-text l-uppercase vh4"><span class="l-bold">Archiv</span><br>Editorial</p>
+						<div class="row">
+							<div class="col-lg-8 col-lg-offset-2">
+								<img class="l-center-img" src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/beispielbilder/belin-1.jpg" alt="">
+							</div><!-- /.col -->
+						</div><!-- /.row -->
+					</a>
+				</div><!-- /.col -->
+					
+				<div class="col-xs-4 archiv-selection-col no-border-lg">
+					<a href="<?php echo esc_url( $link_plakat ); ?>">
+						<p class="l-center-text l-uppercase vh4"><span class="l-bold">Archiv</span><br>Plakate</p>
+						<div class="row">
+							<div class="col-lg-8 col-lg-offset-2">
+								<img class="l-center-img" src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/beispielbilder/belin-2.jpg" alt="">
+							</div><!-- /.col -->
+						</div><!-- /.row -->
+					</a>
+				</div><!-- /.col -->
+					
+				<div class="col-xs-4 archiv-selection-col no-border-lg">
+					<a href="<?php echo esc_url( $link_briefmarken ); ?>">
+						<p class="l-center-text l-uppercase vh4"><span class="l-bold">Archiv</span><br>Briefmarken</p>
+						<div class="row">
+							<div class="col-lg-8 col-lg-offset-2">
+								<img class="l-center-img" src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/beispielbilder/belin-3.jpg" alt="">
+							</div><!-- /.col -->
+						</div><!-- /.row -->
+					</a>
+				</div><!-- /.col -->
+			</div><!-- /.row -->	
 			
+		</div><!-- /.container .archiv-selection -->
+	
 	</div><!-- /#archiv-selection-section -->
+	
+	
 </div><!-- /#contentmain -->
 
 	
--- wp-content/themes/fkc/template-parts/archiv-selection.php
+++ wp-content/themes/fkc/template-parts/archiv-selection.php
@@ -24,52 +24,78 @@ $category_id = get_cat_ID( 'Briefmarken' );
 $link_briefmarken = get_category_link( $category_id );  
 
 
+$queried_object = get_queried_object();
+
+
+$slug_this_page = $queried_object->slug;
+$additional_class_editorial 	= '';
+$additional_class_plakat		= '';
+$additional_class_briefmarken 	= '';
+
+if ( $slug_this_page == 'editorial' ) {
+	$additional_class_plakat		= 'col-xs-offset-2';
+	
+} elseif ( $slug_this_page == 'plakat' ) {
+	$additional_class_editorial 	= 'col-xs-offset-2';
+	
+} elseif ( $slug_this_page == 'briefmarken') {
+	$additional_class_editorial 	= 'col-xs-offset-2';
+}
 ?>
 
 
 		
-<div class="container archiv-selection">
+<div class="container archiv-selection category-page">
 	
 	<div class="row">
 		<a class="is-hidden-lg" href="#"><img class="logo l-center-img" src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/fernkopie-logo.jpg" alt="Fernkopie Logo" title="Fernkopie Logo"></a>
 	</div><!-- /.row -->
 	
 	<div class="row">
-		<div class="col-xs-4 archiv-selection-col">
-			<img class="logo l-center-img is-displayed-lg" src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/fernkopie-logo.jpg" alt="Fernkopie Logo" title="Fernkopie Logo">
-			<a href="<?php echo esc_url( $link_editorial ); ?>">
-				<p class="l-center-text l-uppercase vh4">Archiv Editorial</p>
-				<div class="row">
-					<div class="col-lg-4 col-lg-offset-4">
-						<img class="l-center-img" src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/beispielbilder/belin-1.jpg" alt="">
-					</div><!-- /.col -->
-				</div><!-- /.row -->
-			</a>
-		</div><!-- /.col -->
-			
-		<div class="col-xs-4 archiv-selection-col">
-			<img class="logo l-center-img is-displayed-lg" src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/fernkopie-logo.jpg" alt="Fernkopie Logo" title="Fernkopie Logo">
-			<a href="<?php echo esc_url( $link_plakat ); ?>">
-				<p class="l-center-text l-uppercase vh4">Archiv Plakate</p>
-				<div class="row">
-					<div class="col-lg-4 col-lg-offset-4">
-						<img class="l-center-img" src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/beispielbilder/belin-2.jpg" alt="">
-					</div><!-- /.col -->
-				</div><!-- /.row -->
-			</a>
-		</div><!-- /.col -->
-			
-		<div class="col-xs-4 archiv-selection-col">
-			<img class="logo l-center-img is-displayed-lg" src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/fernkopie-logo.jpg" alt="Fernkopie Logo" title="Fernkopie Logo">
-			<a href="<?php echo esc_url( $link_briefmarken ); ?>">
-				<p class="l-center-text l-uppercase vh4">Archiv Briefmarken</p>
-				<div class="row">
-					<div class="col-lg-4 col-lg-offset-4">
-						<img class="l-center-img" src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/beispielbilder/belin-3.jpg" alt="">
-					</div><!-- /.col -->
-				</div><!-- /.row -->
-			</a>
-		</div><!-- /.col -->
+		
+		<?php if ( $slug_this_page != 'editorial' ) : ?>
+			<div class="row">
+				<div class="col-xs-4 archiv-selection-col <?php echo $additional_class_editorial; ?>">
+					<img class="logo l-center-img is-displayed-lg" src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/fernkopie-logo.jpg" alt="Fernkopie Logo" title="Fernkopie Logo">
+					<a href="<?php echo esc_url( $link_editorial ); ?>">
+						<p class="l-center-text l-uppercase vh4">Archiv Editorial</p>
+						<div class="row">
+							<div class="col-lg-4 col-lg-offset-4">
+								<img class="l-center-img" src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/beispielbilder/belin-1.jpg" alt="">
+							</div><!-- /.col -->
+						</div><!-- /.row -->
+					</a>
+				</div><!-- /.col -->
+		<?php endif; ?>
+				
+		<?php if ( $slug_this_page != 'plakat' ) : ?>
+			<div class="col-xs-4 archiv-selection-col <?php echo $additional_class_plakat; ?>">
+				<img class="logo l-center-img is-displayed-lg" src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/fernkopie-logo.jpg" alt="Fernkopie Logo" title="Fernkopie Logo">
+				<a href="<?php echo esc_url( $link_plakat ); ?>">
+					<p class="l-center-text l-uppercase vh4">Archiv Plakate</p>
+					<div class="row">
+						<div class="col-lg-4 col-lg-offset-4">
+							<img class="l-center-img" src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/beispielbilder/belin-2.jpg" alt="">
+						</div><!-- /.col -->
+					</div><!-- /.row -->
+				</a>
+			</div><!-- /.col -->
+		<?php endif; ?>
+				
+		<?php if ( $slug_this_page != 'briefmarken' ) : ?>		
+			<div class="col-xs-4 archiv-selection-col <?php echo $additional_class_briefmarken; ?>">
+				<img class="logo l-center-img is-displayed-lg" src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/fernkopie-logo.jpg" alt="Fernkopie Logo" title="Fernkopie Logo">
+				<a href="<?php echo esc_url( $link_briefmarken ); ?>">
+					<p class="l-center-text l-uppercase vh4">Archiv Briefmarken</p>
+					<div class="row">
+						<div class="col-lg-4 col-lg-offset-4">
+							<img class="l-center-img" src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/beispielbilder/belin-3.jpg" alt="">
+						</div><!-- /.col -->
+					</div><!-- /.row -->
+				</a>
+			</div><!-- /.col -->
+		<?php endif; ?>
+	
 	</div><!-- /.row -->	
 	
 </div><!-- /.container .archiv-selection -->
