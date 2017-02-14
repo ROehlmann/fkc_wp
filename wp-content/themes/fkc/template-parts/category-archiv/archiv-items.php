60fbbc0dd9a851ffc35b2191c83a99ddd29db646
Rainer Oehlmann <rainer.oehlmann@gmx.de>
2017-02-14 23:23:51 +0100

    Include ordering mechanism to archive category pages

--- wp-content/themes/fkc/template-parts/category-archiv/archiv-items.php
+++ wp-content/themes/fkc/template-parts/category-archiv/archiv-items.php
@@ -1,50 +1,67 @@
 <div id="archive-items-section" class="container">
 	    
-	    <?php if( have_posts() ) : ?>
+    <?php 
+	$my_category = get_query_var('cat');
+
+	$args = array(
+	    'post_type'		 	=> 'archiv',
+	    'cat'				=>  $my_category,
+		'orderby'   		=> 'meta_value_num',
+        'meta_key'			=> 'rank_category_page',
+        'order'    			=> 'DESC'	
+	);
 	    
-		    <div class="row">
-			    
-			    <?php while( have_posts() ) : the_post(); ?>
-			    
-			    	<?php
-					// check if the repeater field has rows of data
-					if( have_rows('repeater') ): ?>
+	//Define the loop based on arguments
+	$query_featured_projects = new WP_Query( $args );
+	
+	//Display the contents
+	if ( $query_featured_projects->have_posts() ) : ?>    
+	    
+	    <div class="row">
+		    
+		    <?php while( $query_featured_projects->have_posts() ) : $query_featured_projects->the_post(); ?>
+		    
+		    	<?php
+				// check if the repeater field has rows of data
+				if( have_rows('repeater') ): ?>
+					
+					<div class="col-xs-6 col-lg-3">
 						
-						<div class="col-xs-6 col-lg-3">
+						<?php // loop through the rows of data
+						while ( have_rows('repeater') ) : the_row(); 
+							$picture = get_sub_field('picture');
+							$customer = get_sub_field('customer');
+							$project_title = get_sub_field('project_title');
+							$year = get_sub_field('year');
+						 ?>
+					
+							<?php if ( !empty($picture) ) : ?>
 							
-							<?php // loop through the rows of data
-							while ( have_rows('repeater') ) : the_row(); 
-								$picture = get_sub_field('picture');
-								$customer = get_sub_field('customer');
-								$project_title = get_sub_field('project_title');
-								$year = get_sub_field('year');
-							 ?>
-						
-								<?php if ( !empty($picture) ) : ?>
+								<a href="<?php echo $picture['url'];?>" data-lightbox="archiv-lightbox">
+									<figure class="gallery-item gallery-item-archiv">
+										<img src="<?php echo $picture['url']; ?>" alt="<?php echo $picture['alt']; ?>" >
+										<figcaption>
+											<h3 class="l-bold vh6"><?php echo $customer; ?></h3>
+											<p class="vh6"><?php echo $project_title; ?></p>
+											<p class="vh6"><?php echo $year;?></p>
+										</figcaption>
+									</figure>
+								</a>
 								
-									<a href="<?php echo $picture['url'];?>" data-lightbox="archiv-lightbox">
-										<figure class="gallery-item gallery-item-archiv">
-											<img src="<?php echo $picture['url']; ?>" alt="<?php echo $picture['alt']; ?>" >
-											<figcaption>
-												<h3 class="l-bold vh6"><?php echo $customer; ?></h3>
-												<p class="vh6"><?php echo $project_title; ?></p>
-												<p class="vh6"><?php echo $year;?></p>
-											</figcaption>
-										</figure>
-									</a>
-									
-							
-								<?php endif; ?>
-		
-							<?php endwhile; ?>
 						
-					    </div><!-- /.col -->
-					    
-					<?php endif; ?>
+							<?php endif; ?>
+	
+						<?php endwhile; ?>
+					
+				    </div><!-- /.col -->
 				    
-			    <?php endwhile; ?>
+				<?php endif; ?>
 			    
-		    </div><!-- /.row -->
-	    
-		<?php endif; ?>
-    </div><!-- /#archive-items-section .container -->
+		    <?php endwhile; ?>
+		    
+	    </div><!-- /.row -->
+    
+	<?php endif; ?>
+	<?php wp_reset_postdata(); ?>
+	
+</div><!-- /#archive-items-section .container -->
